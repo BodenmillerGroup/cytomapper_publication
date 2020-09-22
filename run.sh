2a3057887188dfdb7be7c850898981a48a628b11
nilseling <nils.eling@gmx.de>
2020-09-22 12:45:33 +0000

    Changed docker run tag

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -e PASSWORD=bioc -e ROOT=TRUE -p 8787:8787 -it --cpus="3" --memory="29g" bioconductor_cytomapper:0.0.1
+docker run -e PASSWORD=bioc -e ROOT=TRUE -p 8787:8787 -it --cpus="3" --memory="29g" bioconductor_cytomapper:0.0.2
